42394c45072b4f6220ee4256885f48cda1371f37
Jason Bellegarde <jason.bellegarde@gmail.com>
2020-03-10 01:45:14 -0600

    worked on pyth exp for ncaa model

--- initial.R
+++ initial.R
@@ -2,15 +2,15 @@
 message("starting ncaa project")
 
 # Paths ----
-working.dir <- getwd()
-data.dir <- file.path(working.dir, "data")
-outputs.dir <- file.path(working.dir, "outputs")
-scripts.dir <- file.path(working.dir, "scripts")
+working_dir <- getwd()
+data_dir <- file.path(working_dir, "data")
+outputs_dir <- file.path(working_dir, "outputs")
+scripts_dir <- file.path(working_dir, "scripts")
 
 # Primary functions ----
 
 ## Import packages
-import.packages <- function() {
+import_packages <- function() {
   # Import packages required for scripts
   # Req:
   #   none
@@ -31,39 +31,22 @@ import.packages <- function() {
   suppressPackageStartupMessages(library(tidyverse))
 }
 
-prompt.user <- function(user.prompts, user.title) {
+prompt_user <- function(user_prompts, user_title) {
   # Prompt a user from a multiple choice vector
   # Req:
   #   purrr
   # Arg:
-  #   user.prompts is a vector of length 1 or greater
-  stopifnot(is_character(user.prompts), is_vector(user.prompts),
-            length(user.prompts) >= 1, is_character(user.title),
-            length(user.title) == 1)
+  #   user_prompts is a vector of length 1 or greater
+  stopifnot(
+    is_character(user_prompts), is_vector(user_prompts),
+    length(user_prompts) >= 1, is_character(user_title),
+    length(user_title) == 1
+  )
 
-  temp <- menu(user.prompts, title = user.title)
+  temp <- menu(user_prompts, title = user_title)
 
   return(temp)
 }
 
 # Load packages ----
-import.packages()
-
-# # Update or load datasets ----
-# prompts.vector.update <- c("Update data", "Load data", "Cancel request")
-# prompts.title.update <- "Would you like to update or load datasets?"
-#
-# user.answer.update <- prompt.user(prompts.vector.update, prompts.title.update)
-#
-# if (user.answer.update == 1) {
-#   message("sourcing update data script")
-#   # source(file.path(scripts.dir, "updateData.R"))
-#
-# } else if (user.answer.update == 2) {
-#   message("loading datasets")
-#   # source(file.path(scripts.dir, "loadData.R"))
-#
-# } else (
-#   message("canceling")
-#
-# )
+import_packages()
--- scripts/research.R
+++ scripts/research.R
@@ -1,41 +1,123 @@
 # Start ----
-## Source initial.R before this script
 ## Required packages:
-##  none
+##  testthat
+##  lubridate
+##  tidyverse
 
 # Functions ----
-rmse <- function(error) {
-  sqrt(mean(error^2))
+import_packages <- function() {
+  # Import packages required for research
+  # Req:
+  #   none
+  # Arg:
+  #   none
+  # Return:
+  #   none
+  suppressPackageStartupMessages(library(testthat))
+  suppressPackageStartupMessages(library(lubridate))
+  suppressPackageStartupMessages(library(tidyverse))
 }
 
+rmse <- function(error) {
+  # Finds the root mean square error
+  # Req:
+  #   none
+  # Arg:
+  #   error: the delta between actual and predicted values
+  # Return:
+  #   value as numeric or int
+  as.numeric(sqrt(mean(error^2)))
+}
+
+calc_pyth <- function(points_for, points_against, exponent) {
+  # Calculates the pyth expectations
+  # Req:
+  #   none
+  # Arg:
+  #   goals_for: the goals scored for the team predicted
+  #   goals_against: the goals scoard against the team predicted
+  # Return:
+  #   value as numeric
+  stopifnot(
+    is.numeric(points_for) | is.integer(points_for),
+    is.numeric(points_against) | is.integer(points_against)
+  )
+
+  temp <- 1 / ((1 + (points_against / points_for))^exponent)
+
+  return(temp)
+}
+
+calc_pyth_exp <- function(points_for, points_against, games, exponent) {
+  temp <- (((points_for + points_against) / games) ^ exponent)
+
+  return(temp)
+}
+
+# Import packages ----
+import_packages()
+
+# Paths ----
+working_dir <- getwd()
+data_dir <- file.path(working_dir, "data")
+outputs_dir <- file.path(working_dir, "outputs")
+scripts_dir <- file.path(working_dir, "scripts")
+
 # Data ----
-data.list <- dir(file.path(data.dir, "MDataFiles_Stage1"))
-print(data.list)
-data.reg.compact <- read_csv(
-  file.path(data.dir, "MDataFiles_Stage1/", "MRegularSeasonCompactResults.csv"))
+data_list <- dir(file.path(data_dir, "MDataFiles_Stage1"))
+data_reg_compact <- read_csv(file.path(
+  data_dir,
+  "MDataFiles_Stage1/",
+  "MRegularSeasonCompactResults.csv"
+))
 
 # Research ----
-prediction <- sample((10:1000)/10, 50, replace = TRUE)
+sample_data <- sample(10:1000, 50, replace = TRUE)
+prediction <- sample_data / 10
 actual <- sample(1:100, 50, replace = TRUE)
 
 error <- actual - prediction
 test <- rmse(error)
 
 # Cleaning ----
-df.reg.compact.won <- data.reg.compact %>%
+df_reg_compact_won <- data_reg_compact %>%
   rename(TeamId = WTeamID) %>%
   rename_at(vars(matches("W")), ~ gsub("^(W)", "Team", .)) %>%
   rename_at(vars(matches("L")), ~ gsub("^(L)", "Opp", .)) %>%
   mutate(OppLoc = if_else(TeamLoc == "H", "A",
-                          if_else(TeamLoc == "A", "H", "N")))
+    if_else(TeamLoc == "A", "H", "N")
+  ))
 
-df.reg.compact.loss <- data.reg.compact %>%
+df_reg_compact_loss <- data_reg_compact %>%
   rename(TeamId = LTeamID) %>%
   rename_at(vars(matches("L")), ~ gsub("^(L)", "Team", .)) %>%
   rename_at(vars(matches("W")), ~ gsub("^(W)", "Opp", .)) %>%
   mutate(TeamLoc = if_else(OppLoc == "H", "A",
-                          if_else(OppLoc == "A", "H", "N")))
+    if_else(OppLoc == "A", "H", "N")
+  ))
 
-df.reg.compact <- df.reg.compact.won %>%
-  rbind(df.reg.compact.loss) %>%
+df_reg_compact <- df_reg_compact_won %>%
+  rbind(df_reg_compact_loss)
+
+exp_calc_pyth <- .001
+
+pyth_exp <- calc_pyth_exp(df_reg_compact$TeamScore,
+                          df_reg_compact$OppScore,
+                          1,
+                          exp_calc_pyth)
+
+df_reg_compact <- df_reg_compact %>%
+  mutate(
+    Diff = TeamScore - OppScore,
+    PythExp = calc_pyth(TeamScore, OppScore, pyth_exp)
+  ) %>%
   glimpse()
+
+
+# mutate(
+#   Result = if_else(Diff > 0, 1, if_else(Diff == 0, .5, 0)),
+#   Error = Result - PythExp
+# ) %>%
+#   glimpse()
+#
+# test <- rmse(df_reg_compact$Error)
